08d5dab0926faf6d03161087bb81aac8c5ac1416
Julian Hoch <julian.hoch@hoch-software.ch>
2024-09-28 11:19:55 +0200

    added h/l

--- modes/vtable/evil-collection-vtable.el
+++ modes/vtable/evil-collection-vtable.el
@@ -12,12 +12,16 @@
 (declare-function vtable-previous-column "vtable")
 (declare-function vtable-next-column "vtable")
 
+;; potentially compare with: 
+;; https://github.com/emacs-evil/evil-collection/blob/80a32c46821791c3142da26f471c4e28d0d55916/modes/ztree/evil-collection-ztree.el#L55
+
 (defun evil-collection-vtable-setup ()
   "Set up `evil' bindings for `vtable'."
-  (evil-collection-define-key 'normal 'vtable-header-mode-map
-    (kbd "h") 'vtable-previous-column
-    (kbd "l") 'vtable-next-column)
-  (evil-set-initial-state 'vtable-header-mode 'normal))
+;  (evil-define-minor-mode-key 'normal 'vtable-header-mode-map
+;    (kbd "h") 'vtable-previous-column
+;    (kbd "l") 'vtable-next-column)
+;  (evil-set-initial-state 'vtable-header-mode 'normal))
+  )
 
 (provide 'evil-collection-vtable)
 ;;; evil-collection-vtable.el ends here
